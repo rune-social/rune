b5a62986fda35a27761865d506d1334241b51011
Bombwhale <solkblte@icloud.com>
2025-01-24 15:50:59 -0500

    Feat: add frontend
    - Initialize the frontend app using expo
    - Install prettier, eslint, and related plugins
    - Install NativeWind and tailwindcss

---
+++ frontend/src/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-blue-400">
+      <Text className="text-red-500">Hello, Rune!</Text>
+    </View>
+  );
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,tsx,ts,jsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
