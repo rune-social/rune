1ef31ec74ad3946434693c309d422447258264d6
Bombwhale <solkblte@icloud.com>
2025-01-27 00:40:01 -0500

    Feat: add static serving
    ## Summary
    
    - Add static serving for `/`
    
    ## Test Plan
    
    - Build expo web
    
    ```bash
    cd frontend
    pnpm expo export --platform web
    ```
    
    - Edit `.env`
    
    ```bash
    SERVE_DIR=frontend/dist
    ```
    
    - Start server
    
    ```bash
    cargo run
    ```
    
    - Access webpage and see the result

---
+++ frontend/src/app/(tabs)/about.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function About() {
+  return (
+    <View className="bg-blue-400">
+      <Text className="text-red-500 font-bold">About</Text>
+    </View>
+  );
+}
