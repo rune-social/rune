397972edf599c7089c5b7fea17d6ac86af2c3d0b
JedBeom <20675630+JedBeom@users.noreply.github.com>
2025-01-30 23:15:51 -0500

    Feat: add i18next
    - Install i18next, i18next-react, react-native-localization-settings
    - Add `@types` to autocomplete and lint translation keys
    - Forbid nativewind to edit tsconfig.json
    
    ## Test Plan
    
    ### Web
    
    - Start expo
    - press `w`
    - check the bottom tab bar texts
    - change browser lang (e.g. to Korean) and reset localStorage
    - refresh the website and check the bottom tab bar texts again
    
    ### iOS Simulator
    
    - build ios
    ```
    pnpm ios
    ```
    - check the bottom tab bar texts
    - change simulator language to Korean
    - check text again
    
    ### iOS Device
    
    ```
    pnpm ios --device
    ```
    
    - change the app language preference
    
    
    ## Future Plan
    
    - Add a language selector only for Web

--- frontend/src/app/(tabs)/_layout.tsx
+++ frontend/src/app/(tabs)/_layout.tsx
@@ -1,24 +1,27 @@
 import { IconSymbol } from "@/src/components/IconSymbol";
 import { Tabs } from "expo-router";
+import { useTranslation } from "react-i18next";
 
 export default function TabLayout() {
+  const { t } = useTranslation();
+
   return (
     <Tabs>
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: t("main_menus.home"),
           tabBarIcon: ({ color }) => (
             <IconSymbol name="house.fill" size={32} color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="about"
+        name="more"
         options={{
-          title: "About",
+          title: t("main_menus.more"),
           tabBarIcon: ({ color }) => (
-            <IconSymbol name="house.fill" size={32} color={color} />
+            <IconSymbol name="ellipsis.circle" size={32} color={color} />
           ),
         }}
       />
--- frontend/src/constants/iconMap.ts
+++ frontend/src/constants/iconMap.ts
@@ -5,6 +5,7 @@ export default {
   // See Ionicons here: https://icons.expo.fyi
   // See SF Symbols in the SF Symbols app on Mac.
   "house.fill": "home",
+  "ellipsis.circle": "ellipsis-horizontal-circle",
 } as Partial<
   Record<SymbolViewProps["name"], React.ComponentProps<typeof Ionicons>["name"]>
 >;
