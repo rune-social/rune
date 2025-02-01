7fe6e20bdfd8986d23ff9bf1d3239022a5a9676e
JedBeom <20675630+JedBeom@users.noreply.github.com>
2025-01-31 22:00:32 -0500

    Feat: add diesel-rs
    - Remove sqlx
    - Install diesel-rs and deadpool-diesel
    - Re-write migration files
    - Add mods `schema` and `models`
    
    ## Test Plan
    
    - Install postgresql and edit `DATABASE_URL` in `.env`
    - Run the server
    - Send **POST** http://localhost:8080/test/create_configs
    - GET http://localhost:8080/test/get_configs

---
+++ migrations/2025-01-31-152213_init_stage01/up.sql
@@ -0,0 +1,45 @@
+-- Your SQL goes here
+CREATE TABLE "users"(
+	"id" BIGSERIAL NOT NULL PRIMARY KEY,
+	"username" VARCHAR NOT NULL UNIQUE,
+	"hash" VARCHAR(255) NOT NULL,
+	"totp" VARCHAR(255),
+	"bot_owner_user_id" BIGINT REFERENCES users,
+	"display_name" VARCHAR(255),
+	"bio" TEXT,
+	"created_at" TIMESTAMPTZ NOT NULL,
+	"updated_at" TIMESTAMPTZ NOT NULL,
+	"deleted_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "notes"(
+	"id" BIGSERIAL NOT NULL PRIMARY KEY,
+	"user_id" BIGINT NOT NULL REFERENCES users,
+	"content" TEXT,
+	"reply_of_note_id" BIGINT REFERENCES notes,
+	"renote_of_note_id" BIGINT REFERENCES notes,
+	"edit_of_note_id" BIGINT REFERENCES notes,
+	"created_at" TIMESTAMPTZ NOT NULL,
+	"deleted_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "tokens"(
+	"id" BIGSERIAL NOT NULL PRIMARY KEY,
+	"user_id" BIGINT NOT NULL REFERENCES users,
+	"created_at" TIMESTAMPTZ NOT NULL,
+	"deleted_at" TIMESTAMPTZ
+);
+
+CREATE TABLE "audits"(
+	"id" BIGSERIAL NOT NULL PRIMARY KEY,
+	"token_id" BIGINT NOT NULL REFERENCES tokens,
+	"event" VARCHAR(255) NOT NULL,
+	"detail" JSON,
+	"created_at" TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE "configs"(
+	"id" BIGSERIAL NOT NULL PRIMARY KEY,
+	"key" VARCHAR(255) NOT NULL UNIQUE,
+	"value" TEXT NOT NULL
+);
